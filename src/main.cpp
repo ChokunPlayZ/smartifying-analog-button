5a799520ef3f7df3c8cf11b5d4e1eaaf2e981c3a
ChokunPlayZ <49745982+ChokunPlayZ@users.noreply.github.com>
2021-12-26 14:29:10 +0700

    add code (testing)

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,57 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-void setup() {
-  pinMode(D4, OUTPUT);
-  Serial.begin(115200);
+Servo servo1;
+
+const char *ssid = "Chokun1234 2.4G";
+const char *password = "0635414659";
+
+ESP8266WebServer server(80);
+
+void handleRoot()
+{
+  String s = "Test";
+  server.send(200, "text/html", s);
 }
 
-void loop() {
-  digitalWrite(D4, 1);
-  Serial.println("LED is ON!!!");
-  delay(500);
-  digitalWrite(D4, 0);
-  Serial.println("LED is OFF!!!");
-  delay(500);
+void handleTrig(){
+  servo1.write(1);
+  delay(100);
+  servo1.write(1);
+  Serial.println("Servo Triggered");
+  server.send(200, "text/plane","done");
+}
+
+void setup()
+{
+  servo1.attach(D5);
+  Serial.begin(115200);
+  servo1.write(1);
+  delay(1000);
+
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.on("/t", handleTrig);
+  server.begin();
+}
+
+void loop()
+{
+  server.handleClient();
 }
