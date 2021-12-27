026b49e3561d7dff10820882f39fc243cf1b0c46
ChokunPlayZ <49745982+ChokunPlayZ@users.noreply.github.com>
2021-12-27 14:57:10 +0700

    update site

--- src/html/index.cpp
+++ src/html/index.cpp
@@ -1,3 +1,36 @@
 const char indexpage[] = R"=====(
-    <h1>Title Page</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Button Basher</title>
+</head>
+
+<body>
+    <div class="content">
+        <h2>Button Basher</h2><br>
+        <button onclick="trigger()">Toggle</button>
+    </div>
+</body>
+<script>
+    function trigger() {
+        fetch('/t')
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+</script>
+<style>
+    body {
+        background-color: aqua;
+        color: black;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+
+</html>
 )=====";
